7aec4b24bd04ae22620e257dc82214731090dae0
rivera00255 <rivera00255@gmail.com>
2022-06-06 10:19:13 +0900

    Feat: add config

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const app = express()
-const port = 3000 // 3000번 port로 서버와 연결
+const port = 3000 // 3000번 port로 연결
+
+const config = require('./config/key');
+const {User} = require('./models/User');
 
 // MongoDB에 연결
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://river:1234@boilerplate.oth2l.mongodb.net/?retryWrites=true&w=majority').then(() => console.log('MongoDB connected...'))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err))
+
+// body parser
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+    });
 })
 
 app.listen(port, () => {
