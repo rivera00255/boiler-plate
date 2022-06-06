375efe12e5c86417df71adbd882cb0ed37c4f3dc
rivera00255 <rivera00255@gmail.com>
2022-06-06 12:30:02 +0900

    Feat: add signUp and signIn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000 // 3000번 port로 연결
 
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const {User} = require('./models/User');
 
@@ -14,16 +15,49 @@ mongoose.connect(config.mongoURI).then(() => console.log('MongoDB connected...')
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+// cookie parser
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// signUp
 app.post('/register', (req, res) => {
     const user = new User(req.body);
     user.save((err, doc) => {
         if(err) return res.json({success: false, err})
         return res.status(200).json({success: true})
     });
+})
+
+//signIn
+app.post('/login', (req, res) => {
+    User.findOne({ email: req.body.email}, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message : "존재하지 않는 이메일 정보 입니다."
+            })
+        }
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) {
+                return res.json({
+                    loginSuccess: false,
+                    message : "비밀번호를 다시 확인해주세요."
+                })
+            }
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess : true,
+                    userId: user._id
+                })
+            })
+        })
+    })
 })
 
 app.listen(port, () => {
